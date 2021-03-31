eba27d06c3a5771d75b3f2b7beb1c63473b10612
Isman Fromes <ifromes@gmail.com>
2021-03-31 16:01:51 +0200

    mobile - burger/nav toggle;

--- javascript/bulma.js
+++ javascript/bulma.js
@@ -1,25 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  // Get all "navbar-burger" elements
-  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+    $burger = document.getElementsByClassName('burger')[0];
+    $menu = document.getElementsByClassName('navbar__menu')[0];
 
-  // Check if there are any nav burgers
-  if ($navbarBurgers.length > 0) {
-
-    // Add a click event on each of them
-    $navbarBurgers.forEach(function ($el) {
-      $el.addEventListener('click', () => {
-
-        // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target);
-
-        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
-
-      });
-    });
-  }
-
+    $burger.addEventListener('click', function (e) {
+        e.preventDefault();
+        $burger.classList.toggle('burger--open');
+        $menu.classList.toggle('navbar__menu--open');
+    })
 });
