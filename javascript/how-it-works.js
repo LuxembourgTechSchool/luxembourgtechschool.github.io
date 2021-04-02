6203eea0b9919ddd816da9f5e8a8df6dd8a2c8e8
Isman Fromes <ifromes@gmail.com>
2021-04-02 12:49:35 +0200

    how-it-works sidebar behaviour;

---
+++ javascript/how-it-works.js
@@ -0,0 +1,42 @@
+let navbar_height = document.querySelector('.navbar').offsetHeight;
+
+/**
+ * Header gap calculation
+ */
+let gap = 20;
+
+if (window.innerWidth <= 1215) {
+    gap = navbar_height + 20;
+}
+
+/**
+ * StickySidebar above tablet
+ */
+if (window.innerWidth > 768) {
+    new StickySidebar('.sidenav', {
+        topSpacing: gap,
+        bottomSpacing: 20,
+    });
+}
+
+
+/**
+ Scroll to section after link click
+ */
+document.querySelectorAll('.sidenav__link').forEach(item => {
+    item.addEventListener('click', e => {
+
+        if (document.querySelector(item.hash) != null) {
+            e.preventDefault();
+            const hash = item.hash;
+            window.scroll({
+                top: document.querySelector(hash).offsetTop - gap,
+                left: 0,
+                behavior: 'smooth'
+            });
+
+            history.pushState(hash, '', window.location.pathname + hash);
+        }
+    })
+})
+
