7d1d5825c05139eb3a7a3b8ee7f623cae033f8b2
Isman Fromes <ifromes@gmail.com>
2021-04-13 16:23:04 +0200

    form validation jquery to js;

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,21 +1,24 @@
-/* TODO: convert to vanilla and get rid of jquery */
+document.addEventListener('DOMContentLoaded', function () {
+    const checkCanSubmit = function () {
+        let nameOk = document.querySelector('#name-input').value.length > 0;
+        let emailOk = document.querySelector('#email-input').value.length > 0;
+        let equiryOk = document.querySelector('#enquiry-input').value.length > 0;
+        let messageOk = document.querySelector('#message-input').value.length > 0;
+        let privacyOk = !!document.querySelector('#privacy-input:checked');
 
-$(document).ready(function () {
-    function checkCanSubmit() {
-        let nameOk = $('#name-input').val().length > 0;
-        let emailOk = $('#email-input').val().length > 0;
-        let equiryOk = $('#enquiry-input').val().length > 0;
-        let messageOk = $('#message-input').val().length > 0;
-        let privacyOk = $('#privacy-input').prop('checked');
+        const $submitBtn = document.querySelector('#submit-btn');
 
-        if (nameOk && emailOk && messageOk && equiryOk && privacyOk) {
-            $('#submit-btn').removeAttr('disabled');
+        if (nameOk && emailOk && equiryOk && messageOk && privacyOk) {
+            $submitBtn.removeAttribute('disabled');
         } else {
-            $('#submit-btn').attr('disabled', 'disabled');
+            $submitBtn.setAttribute('disabled', 'disabled')
         }
     }
 
-    $('#name-input, #email-input, #enquiry-input, #message-input, #privacy-input').on('change keyup paste input', function () {
-        checkCanSubmit();
-    });
+    document.querySelectorAll('.contact-form input, .contact-form textarea, .contact-form select').forEach(item => {
+        item.addEventListener('change', checkCanSubmit, false);
+        item.addEventListener('keyup', checkCanSubmit, false);
+        item.addEventListener('paste', checkCanSubmit, false);
+        item.addEventListener('input', checkCanSubmit, false);
+    })
 });
