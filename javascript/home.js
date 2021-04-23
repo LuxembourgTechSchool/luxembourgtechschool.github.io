2735322ea5ab4afa3b6020fed6fc71937739ff27
Isman Fromes <ifromes@gmail.com>
2021-04-23 09:33:04 +0200

    hero mobile video;

--- javascript/home.js
+++ javascript/home.js
@@ -3,8 +3,14 @@ document.addEventListener('DOMContentLoaded', function () {
     /* Adding Video Source after play has been clicked to avoid video being loaded on page load */
     const $home_video = document.querySelector('.js-video');
     const video_url = $home_video.getAttribute('data-url');
+    const video_url_mobile = $home_video.getAttribute('data-url-mobile');
     const $source = document.createElement('source');
-    $source.src = video_url;
+    const break_point = 768;
+    if (window.innerWidth > break_point) {
+        $source.src = video_url;
+    } else {
+        $source.src = video_url_mobile;
+    }
     $source.type = 'video/mp4';
 
     const $play_button = document.querySelector('.js-play');
