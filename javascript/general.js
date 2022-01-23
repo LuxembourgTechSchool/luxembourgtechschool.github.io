cb5640cc7a5afa6c22c09be561fa373ac1b6ef59
Isman Fromes <ifromes@gmail.com>
2022-01-23 13:46:36 +0100

    burger accessibility tweaks;

--- javascript/general.js
+++ javascript/general.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    const $burger = document.getElementsByClassName('burger')[0];
-    const $menu = document.getElementsByClassName('navbar__menu')[0];
+    const $burger = document.getElementsByClassName('js-burger')[0];
+    const $menu = document.getElementsByClassName('js-navbar-menu')[0];
 
     $burger.addEventListener('click', function (e) {
         e.preventDefault();
-        $burger.classList.toggle('burger--open');
-        $menu.classList.toggle('navbar__menu--open');
+        $burger.classList.toggle('open');
+        if ($burger.classList.contains('open')) {
+            $burger.setAttribute('aria-expanded', 'true');
+        } else {
+            $burger.setAttribute('aria-expanded', 'false');
+        }
+        $menu.classList.toggle('open');
     })
 });
